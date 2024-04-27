5ba68e2c68fe0ab7ebd86d622067c748c7105095
Peter Mölzer <moelzerpeter@gmail.com>
2024-04-27 14:59:52 +0100

    commenti

---
+++ fullcode.c
@@ -0,0 +1,779 @@
+// PUSH_SWAP.H
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/libft.h"
+
+// Stack
+typedef struct s_list
+{
+	int				value;
+	int				index;
+	struct s_list	*next;
+}				t_list;
+
+/*In the context of your sorting algorithm project, this t_list structure is 
+likely used to represent the stacks that you need to sort. Each element in 
+the stack is a node in a linked list, with value representing the data to be 
+sorted, index potentially used for keeping track of the node's position or 
+for sorting, and next pointing to the next node in the stack.*/
+
+// Util functions
+t_list	*ft_lstnew(int value);
+t_list	*ft_lstlast(t_list *head);
+void	ft_lstadd_front(t_list **stack, t_list *new);
+void	ft_lstadd_back(t_list **stack, t_list *new);
+void	printList(t_list *head);
+int		ft_lstsize(t_list *head);
+
+void	ft_error(char *msg);
+void	ft_check_args(int argc, char **argv);
+int		is_sorted(t_list **stack);
+int		get_distance(t_list **stack, int index);
+void	make_top(t_list **stack, int distance);
+void	free_stack(t_list **stack);
+void	ft_free(char **str);
+
+// Algorithm utils
+void	radix_sort(t_list **stack_a, t_list **stack_b);
+void	simple_sort(t_list **stack_a, t_list **stack_b);
+void	index_stack(t_list **stack);
+void	sort_5(t_list **stack_a, t_list **stack_b);
+
+// Instruction functions
+int		swap(t_list **stack);
+int		push(t_list **stack_to, t_list **stack_from);
+int		rotate(t_list **stack);
+int		reverseRotate(t_list **stack);
+
+int		sa(t_list **stack_a);
+int		sb(t_list **stack_b);
+int		ss(t_list **stack_a, t_list **stack_b);
+int		pa(t_list **stack_a, t_list **stack_b);
+int		pb(t_list **stack_b, t_list **stack_a);
+int		ra(t_list **stack_a);
+int		rb(t_list **stack_b);
+int		rr(t_list **stack_a, t_list **stack_b);
+int		rra(t_list **stack_a);
+int		rrb(t_list **stack_b);
+int		rrr(t_list **stack_a, t_list **stack_b);
+
+#endif
+
+// INSTRUCTIONS.C
+
+#include "push_swap.h"
+
+// Swaps first two elements of a stack | sa and sb
+
+int	swap(t_list **stack)
+{
+	t_list	*head;
+	t_list	*next;
+	int		tmp_val;
+	int		tmp_index;
+
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
+	head = *stack;
+	next = head->next;
+	if (!head && !next)
+		ft_error("Error occured while swapping!");
+	tmp_val = head->value;
+	tmp_index = head->index;
+	head->value = next->value;
+	head->index = next->index;
+	next->value = tmp_val;
+	next->index = tmp_index;
+	return (0);
+}
+
+int	sa(t_list **stack_a)
+{
+	if (swap(stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("sa", 1);
+	return (0);
+}
+
+int	sb(t_list **stack_b)
+{
+	if (swap(stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("sb", 1);
+	return (0);
+}
+
+int	ss(t_list **stack_a, t_list **stack_b)
+{	
+	if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+		return (-1);
+	swap(stack_a);
+	swap(stack_b);
+	ft_putendl_fd("ss", 1);
+	return (0);
+}
+
+// Takes the first element of one stack and puts it at the top of another | pa and pb
+
+int	push(t_list **stack_to, t_list **stack_from)
+{
+	t_list	*tmp;
+	t_list	*head_to;
+	t_list	*head_from;
+
+	if (ft_lstsize(*stack_from) == 0)
+		return (-1);
+	head_to = *stack_to;
+	head_from = *stack_from;
+	tmp = head_from;
+	head_from = head_from->next;
+	*stack_from = head_from;
+	if (!head_to)
+	{
+		head_to = tmp;
+		head_to->next = NULL;
+		*stack_to = head_to;
+	}
+	else
+	{
+		tmp->next = head_to;
+		*stack_to = tmp;
+	}
+	return (0);
+}
+
+int	pa(t_list **stack_a, t_list **stack_b)
+{
+	if (push(stack_a, stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("pa", 1);
+	return (0);
+}
+
+int	pb(t_list **stack_a, t_list **stack_b)
+{
+	if (push(stack_b, stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("pb", 1);
+	return (0);
+}
+
+// Shift up all elements of a stack by 1. The first element becomes the last one | ra and rb
+
+int	rotate(t_list **stack)
+{
+	t_list	*head;
+	t_list	*tail;
+
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
+	head = *stack;
+	tail = ft_lstlast(head);
+	*stack = head->next;
+	head->next = NULL;
+	tail->next = head;
+	return (0);
+}
+
+int	ra(t_list **stack_a)
+{
+	if (rotate(stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("ra", 1);
+	return (0);
+}
+
+int	rb(t_list **stack_b)
+{
+	if (rotate(stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("rb", 1);
+	return (0);
+}
+
+int	rr(t_list **stack_a, t_list **stack_b)
+{
+	if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+		return (-1);
+	rotate(stack_a);
+	rotate(stack_b);
+	ft_putendl_fd("rr", 1);
+	return (0);
+}
+
+
+// Shifts down all elements of a stack by 1. The last element becomes the first one | rra and rrb
+
+int	reverseRotate(t_list **stack)
+{
+	t_list	*head;
+	t_list	*tail;
+
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
+	head = *stack;
+	tail = ft_lstlast(head);
+	while (head)
+	{
+		if (head->next->next == NULL)
+		{
+			 head->next = NULL;
+			 break ;
+		}
+		head = head->next;
+	}
+	tail->next = *stack;
+	*stack = tail;
+	return (0);
+}
+
+int	rra(t_list **stack_a)
+{
+	if (reverseRotate(stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("rra", 1);
+	return (0);
+}
+
+int	rrb(t_list **stack_b)
+{
+	if (reverseRotate(stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("rrb", 1);
+	return (0);
+}
+
+int	rrr(t_list **stack_a, t_list **stack_b)
+{
+	if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+		return (-1);
+	reverseRotate(stack_a);
+	reverseRotate(stack_b);
+	ft_putendl_fd("rrr", 1);
+	return (0);
+}
+
+
+// MAIN.C
+
+#include "push_swap.h"
+
+static void	initStack(t_list **stack, int argc, char **argv)
+{
+	t_list	*new;
+	char	**args;
+	int		i;
+
+	i = 0;
+	if (argc == 2)
+		args = ft_split(argv[1], ' ');
+	else
+	{
+		i = 1;
+		args = argv;
+	}
+	while (args[i])
+	{
+		new = ft_lstnew(ft_atoi(args[i]));
+		ft_lstadd_back(stack, new);
+		i++;
+	}
+	index_stack(stack);
+	if (argc == 2)
+		ft_free(args);
+}
+
+static void	sort_stack(t_list **stack_a, t_list **stack_b)
+{
+	if (ft_lstsize(*stack_a) <= 5)
+		simple_sort(stack_a, stack_b);
+	else
+		radix_sort(stack_a, stack_b);
+}
+
+int	main(int argc, char **argv)
+{
+	t_list	**stack_a;
+	t_list	**stack_b;
+	/*Passing single pointers would make it difficult to update the head of 
+	the list in the calling function. With double pointers, the functions 
+	can directly change where the pointers point to, allowing them to modify the stacks*/
+
+	if (argc < 2)
+		return (-1);
+	ft_check_args(argc, argv);
+	stack_a = (t_list **)malloc(sizeof(t_list));
+	stack_b = (t_list **)malloc(sizeof(t_list));
+	*stack_a = NULL;
+	*stack_b = NULL;
+	initStack(stack_a, argc, argv);
+	if (is_sorted(stack_a))
+	{
+		free_stack(stack_a);
+		free_stack(stack_b);
+		return (0);
+	}
+	sort_stack(stack_a, stack_b);
+	free_stack(stack_a);
+	free_stack(stack_b);
+	return (0);
+}
+
+// RADIX.C
+
+#include "push_swap.h"
+
+static int	get_max_bits(t_list **stack)
+{
+	t_list	*head;
+	int		max;
+	int		max_bits;
+
+	head = *stack;
+	max = head->index;
+	max_bits = 0;
+	while (head)
+	{
+		if (head->index > max)
+			max = head->index;
+		head = head->next;
+	}
+	while ((max >> max_bits) != 0)
+		max_bits++;
+	return (max_bits);
+}
+
+void	radix_sort(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*head_a;
+	int		i;
+	int		j;
+	int		size;
+	int		max_bits;
+
+	i = 0;
+	head_a = *stack_a;
+	size = ft_lstsize(head_a);
+	max_bits = get_max_bits(stack_a);
+	while (i < max_bits)
+	{
+		j = 0;
+		while (j++ < size)
+		{
+			head_a = *stack_a;
+			if (((head_a->index >> i) & 1) == 1)
+				ra(stack_a);
+			else
+				pb(stack_a, stack_b);
+		}
+		while (ft_lstsize(*stack_b) != 0)
+			pa(stack_a, stack_b);
+		i++;
+	}
+}
+
+// SIMPLE.C
+
+#include "push_swap.h"
+
+static int	get_min(t_list **stack, int val)
+{
+	t_list	*head;
+	int		min;
+
+	head = *stack;
+	min = head->index;
+	while (head->next)
+	{
+		head = head->next;
+		if ((head->index < min) && head->index != val)
+			min = head->index;
+	}
+	return (min);
+}
+
+static void	sort_3(t_list **stack_a)
+{
+	t_list	*head;
+	int		min;
+	int		next_min;
+
+	head = *stack_a;
+	min = get_min(stack_a, -1);
+	next_min = get_min(stack_a, min);
+	if (is_sorted(stack_a))
+		return ;
+	if (head->index == min && head->next->index != next_min)
+	{
+		ra(stack_a);
+		sa(stack_a);
+		rra(stack_a);
+	}
+	else if (head->index == next_min)
+	{
+		if (head->next->index == min)
+			sa(stack_a);
+		else
+			rra(stack_a);
+	}
+	else
+	{
+		if (head->next->index == min)
+			ra(stack_a);
+		else
+		{
+			sa(stack_a);
+			rra(stack_a);
+		}
+	}
+}
+
+static void	sort_4(t_list **stack_a, t_list **stack_b)
+{
+	int	distance;
+
+	if (is_sorted(stack_a))
+		return ;
+	distance = get_distance(stack_a, get_min(stack_a, -1));
+	if (distance == 1)
+		ra(stack_a);
+	else if (distance == 2)
+	{
+		ra(stack_a);
+		ra(stack_a);
+	}
+	else if (distance == 3)
+		rra(stack_a);
+	if (is_sorted(stack_a))
+		return ;
+	pb(stack_a, stack_b);
+	sort_3(stack_a);
+	pa(stack_a, stack_b);
+}
+
+void	sort_5(t_list **stack_a, t_list **stack_b)
+{
+	int	distance;
+
+	distance = get_distance(stack_a, get_min(stack_a, -1));
+	if (distance == 1)
+		ra(stack_a);
+	else if (distance == 2)
+	{
+		ra(stack_a);
+		ra(stack_a);
+	}
+	else if (distance == 3)
+	{
+		rra(stack_a);
+		rra(stack_a);
+	}
+	else if (distance == 4)
+		rra(stack_a);
+	if (is_sorted(stack_a))
+		return ;
+	pb(stack_a, stack_b);
+	sort_4(stack_a, stack_b);
+	pa(stack_a, stack_b);
+}
+
+void	simple_sort(t_list **stack_a, t_list **stack_b)
+{
+	int	size;
+
+	if (is_sorted(stack_a) || ft_lstsize(*stack_a) == 0
+		|| ft_lstsize(*stack_a) == 1)
+		return ;
+	size = ft_lstsize(*stack_a);
+	if (size == 2)
+		sa(stack_a);
+	else if (size == 3)
+		sort_3(stack_a);
+	else if (size == 4)
+		sort_4(stack_a, stack_b);
+	else if (size == 5)
+		sort_5(stack_a, stack_b);
+}
+
+// CHECKER.C 
+
+#include "push_swap.h"
+
+static int	ft_contains(int num, char **argv, int i)
+{
+	i++;
+	while (argv[i])
+	{
+		if (ft_atoi(argv[i]) == num)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	ft_isnum(char *num)
+{
+	int	i;
+
+	i = 0;
+	if (num[0] == '-')
+		i++;
+	while (num[i])
+	{
+		if (!ft_isdigit(num[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_check_args(int argc, char **argv)
+{
+	int		i;
+	long	tmp;
+	char	**args;	
+
+	i = 0;
+	if (argc == 2)
+		args = ft_split(argv[1], ' ');
+	else
+	{
+		i = 1;
+		args = argv;
+		/*it sets i to 1 (to skip the program name) and 
+		directly uses argv as the args array.*/
+	}
+	while (args[i])
+	{
+		tmp = ft_atoi(args[i]);
+		if (!ft_isnum(args[i]))
+			ft_error("Error");
+		if (ft_contains(tmp, args, i))
+			ft_error("Error");
+		if (tmp < -2147483648 || tmp > 2147483647)
+			ft_error("Error");
+		i++;
+	}
+	if (argc == 2)
+		ft_free(args);
+}
+
+// INDEX.C
+
+#include "push_swap.h"
+
+static t_list	*get_next_min(t_list **stack)
+{
+	t_list	*head;
+	t_list	*min;
+	int		has_min;
+
+	min = NULL;
+	has_min = 0;
+	head = *stack;
+	if (head)
+	{
+		while (head)
+		{
+			if ((head->index == -1) && (!has_min || head->value < min->value))
+			{
+				min = head;
+				has_min = 1;
+			}
+			head = head->next;
+		}
+	}
+	return (min);
+}
+
+void	index_stack(t_list **stack)
+{
+	t_list	*head;
+	int		index;
+
+	index = 0;
+	head = get_next_min(stack);
+	while (head)
+	{
+		head->index = index++;
+		head = get_next_min(stack);
+	}
+}
+
+// T_LIST.C
+
+#include "push_swap.h"
+
+// Creates new node and returns the pointer of it
+t_list	*ft_lstnew(int value)
+{
+	t_list	*new;
+
+	new = (t_list *) malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->value = value;
+	new->index = -1;
+	new->next = NULL;
+	return (new);
+}
+
+// Adds the specified node to a stack (list) making it the head
+void	ft_lstadd_front(t_list **stack, t_list *new)
+{
+	new->next = *stack;
+	*stack = new;
+}
+
+// Returns the last node of a list 
+t_list	*ft_lstlast(t_list *head)
+{
+	t_list	*tmp;
+
+	tmp = head;
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+		if (tmp->next == NULL)
+			return (tmp);
+	}
+	return (tmp);
+}
+
+// Adds the specified node to a stack (list) making it the last node
+void	ft_lstadd_back(t_list **stack, t_list *new)
+{
+	t_list	*n;
+
+	if (*stack)
+	{
+		n = ft_lstlast(*stack);
+		n->next = new;
+		new->next = NULL;
+	}
+	else
+	{
+		*stack = new;
+		(*stack)->next = NULL;
+	}
+}
+
+// Returns the size of the Linked List
+int	ft_lstsize(t_list *head)
+{
+	size_t	i;
+	t_list	*tmp;
+
+	tmp = head;
+	i = 0;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
+
+// Prints the Linked List
+void	printList(t_list *head)
+{
+	t_list	*tmp;
+
+	tmp = head;
+	while (tmp != NULL)
+	{
+		ft_putnbr_fd(tmp->value, 1);
+		ft_putendl_fd("", 1);
+		tmp = tmp->next;
+	}
+}
+
+// UTILS.C
+
+#include "push_swap.h"
+
+void	ft_error(char *msg)
+{
+	ft_putendl_fd(msg, 1);
+	exit(0);
+}
+
+void	ft_free(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	while (i >= 0)
+		free(str[i--]);
+}
+
+int	is_sorted(t_list **stack)
+{
+	t_list	*head;
+
+	head = *stack;
+	while (head && head->next)
+	{
+		if (head->value > head->next->value)
+			return (0);
+		head = head->next;
+	}
+	return (1);
+}
+
+int	get_distance(t_list **stack, int index)
+{
+	t_list	*head;
+	int		distance;
+
+	distance = 0;
+	head = *stack;
+	while (head)
+	{
+		if (head->index == index)
+			break ;
+		distance++;
+		head = head->next;
+	}
+	return (distance);
+}
+
+void	make_top(t_list **stack, int distance)
+{
+	t_list	*head;
+	int		tmp;
+
+	if (distance == 0)
+		return ;
+	head = *stack;
+	tmp = ft_lstsize(head) - distance;
+	if (distance <= (ft_lstsize(head) / 2))
+	{
+		while (distance-- > 0)
+			ra(stack);
+	}
+	else
+	{
+		while (tmp-- > 0)
+			rra(stack);
+	}
+}
+
+void	free_stack(t_list **stack)
+{
+	t_list	*head;
+	t_list	*tmp;
+
+	head = *stack;
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	free(stack);
+}
---
+++ things_to_do.c
@@ -0,0 +1,7 @@
+1. change name of structure from t_list to stack_list
+
+
+
+dove sono:
+
+int	main(int argc, char **argv)
